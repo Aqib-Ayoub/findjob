1766e69663e3918a6eea0d4866f6e9ade378def3
Aqib-Ayoub <aqibayoub321@gmail.com>
2025-07-04 21:39:14 +0530

    changed it to localost

--- lib/services/config.dart
+++ lib/services/config.dart
@@ -1,5 +1,7 @@
 class Config {
-  static const apiUrl = "findjobbackend-production-3d25.up.railway.app";
+  // static const apiUrl = "findjobbackend-production-3d25.up.railway.app";
+  // static const apiUrl = "http://192.168.1.8:5003/api/jobs";
+  static const apiUrl = "10.0.2.2:5003";
   static const String loginUrl = "/api/login";
   static const String signupUrl = "/api/register";
   static const String jobs = "/api/jobs";

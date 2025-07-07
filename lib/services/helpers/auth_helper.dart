f286165e1ad9b9445d3bb343c79b138e0565c28d
Aqib-Ayoub <aqibayoub321@gmail.com>
2025-07-07 21:14:39 +0530

    Refined login and job detail screens for better UI/UX

--- lib/services/helpers/auth_helper.dart
+++ lib/services/helpers/auth_helper.dart
@@ -1,6 +1,24 @@
+import 'package:findjob/services/config.dart';
 import 'package:http/http.dart' as https;
 
 class AuthHelper {
   static var client = https.Client();
-
+  static Future<bool> signup(model) async {
+    try {
+      Map<String, String> requestHeaders = {'Content-Type': 'application/json'};
+      var url = Uri.https(Config.apiUrl, Config.signupUrl);
+      var response = await client.post(
+        url,
+        headers: requestHeaders,
+        body: model,
+      );
+      if (response.statusCode == 201) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      return false;
+    }
+  }
 }
--- lib/views/screens/auth/login.dart
+++ lib/views/screens/auth/login.dart
@@ -6,6 +6,7 @@ import 'package:findjob/views/common/custom_btn.dart';
 import 'package:findjob/views/common/custom_textfield.dart';
 import 'package:findjob/views/common/exports.dart';
 import 'package:findjob/views/common/height_spacer.dart';
+import 'package:findjob/views/common/pages_loader.dart';
 import 'package:findjob/views/common/styled_container.dart';
 import 'package:findjob/views/common/width_spacer.dart';
 import 'package:findjob/views/screens/auth/signup.dart';
@@ -53,106 +54,113 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
           ),
-          body: buildStyledContainer(
-            context,
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.w),
-              child: Form(
-                child: ListView(
-                  padding: EdgeInsets.zero,
-                  children: [
-                    HeightSpacer(size: 50.h),
-                    ReusableText(
-                      text: 'Welcome Back',
-                      style: appstyle(30, kDark, FontWeight.w600),
-                    ),
-                    ReusableText(
-                      text: 'Fill in the Details to login your account',
-                      style: appstyle(
-                        14,
-                        Color(kDarkGrey.value),
-                        FontWeight.w400,
-                      ),
-                    ),
-                    HeightSpacer(size: 40.h),
-                    CustomTextField(
-                      controller: email,
-                      hintText: 'Enter your email',
-                      keyboardType: TextInputType.emailAddress,
-                      validator: (email) {
-                        if (email!.isEmpty || !email.contains('@')) {
-                          return 'Please entter a valid email';
-                        }
-                        return null;
-                      },
-                    ),
-                    HeightSpacer(size: 20.h),
-                    CustomTextField(
-                      controller: password,
-                      hintText: 'Password',
-                      keyboardType: TextInputType.text,
-                      obscureText: loginNotifier.obscureText,
-                      suffixIcon: GestureDetector(
-                        onTap: () {
-                          loginNotifier.obscureText =
-                              !loginNotifier.obscureText;
-                        },
-                        child: Icon(
-                          loginNotifier.obscureText
-                              ? Icons.visibility_off
-                              : Icons.visibility,
+          body:
+              loginNotifier.loader
+                  ? PageLoader()
+                  : buildStyledContainer(
+                    context,
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20.w),
+                      child: Form(
+                        child: ListView(
+                          padding: EdgeInsets.zero,
+                          children: [
+                            HeightSpacer(size: 50.h),
+                            ReusableText(
+                              text: 'Welcome Back',
+                              style: appstyle(30, kDark, FontWeight.w600),
+                            ),
+                            ReusableText(
+                              text: 'Fill in the Details to login your account',
+                              style: appstyle(
+                                14,
+                                Color(kDarkGrey.value),
+                                FontWeight.w400,
+                              ),
+                            ),
+                            HeightSpacer(size: 40.h),
+                            CustomTextField(
+                              controller: email,
+                              hintText: 'Enter your email',
+                              keyboardType: TextInputType.emailAddress,
+                              validator: (email) {
+                                if (email!.isEmpty || !email.contains('@')) {
+                                  return 'Please entter a valid email';
+                                }
+                                return null;
+                              },
+                            ),
+                            HeightSpacer(size: 20.h),
+                            CustomTextField(
+                              controller: password,
+                              hintText: 'Password',
+                              keyboardType: TextInputType.text,
+                              obscureText: loginNotifier.obscureText,
+                              suffixIcon: GestureDetector(
+                                onTap: () {
+                                  loginNotifier.obscureText =
+                                      !loginNotifier.obscureText;
+                                },
+                                child: Icon(
+                                  loginNotifier.obscureText
+                                      ? Icons.visibility_off
+                                      : Icons.visibility,
+                                ),
+                              ),
+                              validator: (password) {
+                                if (password!.isEmpty || password.length < 8) {
+                                  return 'Please enter a valid password';
+                                } else {
+                                  return null;
+                                }
+                              },
+                            ),
+                            HeightSpacer(size: 10.h),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  ReusableText(
+                                    text: 'Don\'t have an account',
+                                    style: appstyle(
+                                      11,
+                                      Color(kDarkGrey.value),
+                                      FontWeight.w500,
+                                    ),
+                                  ),
+                                  WidthSpacer(width: 5.w),
+                                  GestureDetector(
+                                    onTap: () {
+                                      Get.offAll(() => RegistrationPage());
+                                    },
+                                    child: ReusableText(
+                                      text: 'Register',
+                                      style: appstyle(
+                                        12,
+                                        kLightBlue,
+                                        FontWeight.w600,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            HeightSpacer(size: 30.h),
+                            Consumer<ZoomNotifier>(
+                              builder: (context, zoomNotifier, child) {
+                                return CustomButton(
+                                  text: 'Login',
+                                  //TODO: Implement login functionality
+                                  onTap: () {},
+                                );
+                              },
+                            ),
+                          ],
                         ),
                       ),
-                      validator: (password) {
-                        if (password!.isEmpty || password.length < 8) {
-                          return 'Please enter a valid password';
-                        } else {
-                          return null;
-                        }
-                      },
                     ),
-                    HeightSpacer(size: 10.h),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          ReusableText(
-                            text: 'Don\'t have an account',
-                            style: appstyle(
-                              11,
-                              Color(kDarkGrey.value),
-                              FontWeight.w500,
-                            ),
-                          ),
-                          WidthSpacer(width: 5.w),
-                          GestureDetector(
-                            onTap: () {
-                              Get.offAll(() => RegistrationPage());
-                            },
-                            child: ReusableText(
-                              text: 'Register',
-                              style: appstyle(12, kLightBlue, FontWeight.w600),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    HeightSpacer(size: 30.h),
-                    Consumer<ZoomNotifier>(
-                      builder: (context, zoomNotifier, child) {
-                        return CustomButton(
-                          text: 'Login',
-                          //TODO: Implement login functionality
-                          onTap: () {},
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+                  ),
         );
       },
     );

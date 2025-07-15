194afd74775009ae3de6dfe5dbc0511de63d5ac2
Aqib-Ayoub <aqibayoub321@gmail.com>
2025-07-15 21:59:15 +0530

    improved profile ui

--- lib/views/screens/auth/profile.dart
+++ lib/views/screens/auth/profile.dart
@@ -1,19 +1,23 @@
 import 'package:findjob/constants/app_constants.dart';
 import 'package:findjob/controllers/exports.dart';
-import 'package:findjob/controllers/login_provider.dart';
 import 'package:findjob/models/response/auth/profile_model.dart';
 import 'package:findjob/services/helpers/auth_helper.dart';
 import 'package:findjob/views/common/app_bar.dart';
+import 'package:findjob/views/common/app_style.dart';
 import 'package:findjob/views/common/back_btn.dart';
 import 'package:findjob/views/common/cached_network_image.dart';
 import 'package:findjob/views/common/custom_outline_btn.dart';
 import 'package:findjob/views/common/drawer/drawer_widget.dart';
+import 'package:findjob/views/common/height_spacer.dart';
 import 'package:findjob/views/common/pages_loader.dart';
+import 'package:findjob/views/common/reusable_text.dart';
 import 'package:findjob/views/common/styled_container.dart';
+import 'package:findjob/views/common/width_spacer.dart';
 import 'package:findjob/views/screens/auth/non_user.dart';
 import 'package:findjob/views/screens/mainscreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:provider/provider.dart';
@@ -93,11 +97,12 @@ class _ProfilePageState extends State<ProfilePage> {
                     return buildStyledContainer(
                       context,
                       ListView(
-                        padding: EdgeInsets.symmetric(horizontal: 20.w),
+                        padding: EdgeInsets.symmetric(horizontal: 12.w),
                         children: [
                           Container(
+                            padding: EdgeInsets.symmetric(horizontal: 20),
                             width: width,
-                            height: hieght * 0.1,
+                            height: hieght * 0.07,
                             decoration: BoxDecoration(
                               color: Color(kGreen.value),
                               borderRadius: BorderRadius.all(
@@ -106,9 +111,48 @@ class _ProfilePageState extends State<ProfilePage> {
                             ),
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [],
+                              children: [
+                                Row(
+                                  children: [
+                                    CircularAvata(
+                                      image: profile!.profile,
+                                      w: 50,
+                                      h: 50,
+                                    ),
+                                    WidthSpacer(width: 20.w),
+                                    Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        ReusableText(
+                                          text: profile.username,
+                                          style: appstyle(
+                                            14,
+                                            kDarkGrey,
+                                            FontWeight.w400,
+                                          ),
+                                        ),
+                                        ReusableText(
+                                          text: profile.email,
+                                          style: appstyle(
+                                            14,
+                                            Color(kDarkGrey.value),
+                                            FontWeight.w400,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                Icon(Feather.edit),
+                              ],
                             ),
                           ),
+
+                          HeightSpacer(size: 20),
+
                           CustomOutlineBtn(
                             onTab: () {
                               zoomNotifier.currentIndex = 0;

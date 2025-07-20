580080827ce77479f1221ac9a2ce763fbb4b4bea
Aqib-Ayoub <aqibayoub321@gmail.com>
2025-07-20 18:42:23 +0530

    added job upload UI and backend functionality

--- lib/views/screens/jobs/add_jobs.dart
+++ lib/views/screens/jobs/add_jobs.dart
@@ -1,9 +1,12 @@
 import 'package:findjob/constants/app_constants.dart';
 import 'package:findjob/views/common/app_bar.dart';
 import 'package:findjob/views/common/back_btn.dart';
+import 'package:findjob/views/common/custom_outline_btn.dart';
+import 'package:findjob/views/common/exports.dart';
 import 'package:findjob/views/common/height_spacer.dart';
 import 'package:findjob/views/common/styled_container.dart';
 import 'package:findjob/views/common/textfield.dart';
+import 'package:findjob/views/screens/auth/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -21,12 +24,14 @@ class _AddJobsState extends State<AddJobs> {
   TextEditingController salary = TextEditingController();
   TextEditingController contract = TextEditingController();
   TextEditingController description = TextEditingController();
+  TextEditingController period = TextEditingController();
   TextEditingController imageUrl = TextEditingController();
   TextEditingController rq1 = TextEditingController();
   TextEditingController rq2 = TextEditingController();
   TextEditingController rq3 = TextEditingController();
   TextEditingController rq4 = TextEditingController();
   TextEditingController rq5 = TextEditingController();
+  String image = 'https://avatars.githubusercontent.com/u/52028089?v=4';
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +40,14 @@ class _AddJobsState extends State<AddJobs> {
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(50.w),
         child: CustomAppBar(
+          action: [
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: CircularAvata(image: image, w: 30, h: 40),
+            ),
+          ],
           color: Color(kLightBlue.value),
+
           text: 'Upload jobs',
           child: BackBtn(),
         ),
@@ -136,11 +148,140 @@ class _AddJobsState extends State<AddJobs> {
                             }
                           },
                           controller: description,
+                          hintText: 'Image Url',
+                          label: Text('Image Url'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 6.0),
+                        child: buildtextfield(
+                          onSubmitted: (value) {
+                            if (value!.isEmpty) {
+                              return ("Please fill the field");
+                            } else {
+                              return null;
+                            }
+                          },
+                          controller: description,
                           hintText: 'Description',
                           label: Text('Desscription'),
                           maxLines: 3,
                           height: 100,
                         ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 6.0),
+                        child: buildtextfield(
+                          onSubmitted: (value) {
+                            if (value!.isEmpty) {
+                              return ("Please fill the field");
+                            } else {
+                              return null;
+                            }
+                          },
+                          controller: period,
+                          hintText: 'Monthly | Annual | Weakly',
+                          label: Text('Salary period'),
+                          maxLines: 2,
+                          height: 100,
+                        ),
+                      ),
+                      ReusableText(
+                        text: 'Requirements',
+                        style: appstyle(15, Colors.black, FontWeight.w600),
+                      ),
+                      HeightSpacer(size: 10),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 6.0),
+                        child: buildtextfield(
+                          height: 80,
+                          maxLines: 2,
+                          onSubmitted: (value) {
+                            if (value!.isEmpty) {
+                              return ("Please fill the field");
+                            } else {
+                              return null;
+                            }
+                          },
+                          controller: rq1,
+                          hintText: 'Requirements',
+                          label: Text('Requirements'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 6.0),
+                        child: buildtextfield(
+                          height: 80,
+                          maxLines: 2,
+                          onSubmitted: (value) {
+                            if (value!.isEmpty) {
+                              return ("Please fill the field");
+                            } else {
+                              return null;
+                            }
+                          },
+                          controller: rq2,
+                          hintText: 'Requirements',
+                          label: Text('Requirements'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 6.0),
+                        child: buildtextfield(
+                          height: 80,
+                          maxLines: 2,
+                          onSubmitted: (value) {
+                            if (value!.isEmpty) {
+                              return ("Please fill the field");
+                            } else {
+                              return null;
+                            }
+                          },
+                          controller: rq3,
+                          hintText: 'Requirements',
+                          label: Text('Requirements'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 6.0),
+                        child: buildtextfield(
+                          height: 80,
+                          maxLines: 2,
+                          onSubmitted: (value) {
+                            if (value!.isEmpty) {
+                              return ("Please fill the field");
+                            } else {
+                              return null;
+                            }
+                          },
+                          controller: rq4,
+                          hintText: 'Requirements',
+                          label: Text('Requirements'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 6.0),
+                        child: buildtextfield(
+                          height: 80,
+                          maxLines: 3,
+                          onSubmitted: (value) {
+                            if (value!.isEmpty) {
+                              return ("Please fill the field");
+                            } else {
+                              return null;
+                            }
+                          },
+                          controller: rq5,
+                          hintText: 'Requirements',
+                          label: Text('Requirements'),
+                        ),
+                      ),
+                      CustomOutlineBtn(
+                        height: 45.h,
+                        width: width,
+                        onTab: () {},
+                        text: 'Submit',
+                        color: Color(kLightBlue.value),
                       ),
                     ],
                   ),
